cd557c426d83134319dec97a89c5d0fd2d17f1a8
Helen Gezahegn <gezahegn@ualberta.ca>
2023-10-09 21:46:10 -0600

    feat: added header + footer - render header and footer in page.tsx - added btnType to CustomButtonProps interface - added footer link constants to index.ts

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,6 @@
 import Hero from "./hero";
 import CustomButton from "./custom-button";
+import NavBar from "./nav-bar";
+import Footer from "./footer";
 
-export { Hero, CustomButton };
+export { Hero, CustomButton, NavBar, Footer };
--- types/index.ts
+++ types/index.ts
@@ -3,6 +3,7 @@ import { MouseEventHandler } from "react";
 // ? means optional
 export interface CustomButtonProps {
   title: string;
+  btnType?: "button" | "submit";
   containerStyles?: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
 }
