efdf9b48eefab6fdf2fcc3479b838cc37e7d3c94
Helen Gezahegn <gezahegn@ualberta.ca>
2023-10-10 19:23:19 -0600

    fix: keep track of selected manufacturer and call setManufacturer

--- components/search-manufacturer.tsx
+++ components/search-manufacturer.tsx
@@ -25,7 +25,7 @@ const SearchManufacturer = ({
 
   return (
     <div className="search-manufacturer">
-      <Combobox>
+      <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className="relative w-full">
           <Combobox.Button className="absolute top-[14px]">
             <Image
@@ -64,7 +64,26 @@ const SearchManufacturer = ({
                     }`
                   }
                 >
-                  {item}
+                  {({ selected, active }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? "font-medium" : "font-normal"
+                        }`}
+                      >
+                        {item}
+                      </span>
+
+                      {/* Show an active blue background color if the option is selected */}
+                      {selected ? (
+                        <span
+                          className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                            active ? "text-white" : "text-pribg-primary-purple"
+                          }`}
+                        ></span>
+                      ) : null}
+                    </>
+                  )}
                 </Combobox.Option>
               ))}
             </Combobox.Options>
