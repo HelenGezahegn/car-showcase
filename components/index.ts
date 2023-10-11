cda4f4b889894dbd5e408db888a415d6a5463873
Helen Gezahegn <gezahegn@ualberta.ca>
2023-10-10 19:10:39 -0600

    feat: add search bar for car manufacturer - Add section header for Car Catalogue where cars will be displayed - added search-bar under header - within search bar, added search-manufacturer to filter user input as they search for a car manufacturer - used headless to display bar and transitions

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,16 @@ import Hero from "./hero";
 import CustomButton from "./custom-button";
 import NavBar from "./nav-bar";
 import Footer from "./footer";
+import SearchBar from "./search-bar";
+// import CustomFilter from "./custom-filter";
+import SearchManufacturer from "./search-manufacturer";
 
-export { Hero, CustomButton, NavBar, Footer };
+export {
+  Hero,
+  CustomButton,
+  NavBar,
+  Footer,
+  SearchBar,
+  // CustomFilter,
+  SearchManufacturer
+};
